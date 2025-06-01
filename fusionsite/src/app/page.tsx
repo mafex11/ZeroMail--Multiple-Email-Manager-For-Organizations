1c407f197e9335effcb66f89cc569218b44a629e
Sudhanshu Pandit <sudhanshuk1140@gmail.com>
2025-06-01 23:03:38 +0530

    some starting changes

--- fusionsite/src/app/page.tsx
+++ fusionsite/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
-
+import { Cover } from "@/Components/ui/cover";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        Main
+      <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
+        Build amazing websites <br /> at <Cover>warp speed</Cover>
+      </h1>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
        Footer
